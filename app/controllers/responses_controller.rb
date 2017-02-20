9f8c47073d4dbfadb2ad4cacdc952b8ff09c0721
Ezra Sandzer-Bell and Jonathan Thom <ezra+jonathan@>
2017-02-20 11:56:44 -0800

    Add user auth

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,7 +1,7 @@
 class ResponsesController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
-    @response = @question.responses.new(option_1: params[:response][:option_1], option_2: params[:response][:option_2])
+    @response = @question.responses.new(option_1: params[:response][:option_1], option_2: params[:response][:option_2], user_id: current_user.id)
     if @response.save
       flash[:notice] = "You voted!"
       respond_to do |format|
