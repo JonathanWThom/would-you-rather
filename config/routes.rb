26371166d7f7bd4d30ba0c68f1aafaa1e5e551fc
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-20 08:48:30 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'questions#index'
+  resources :questions do
+    resources :responses
+  end
+
+  #do I need nested routes?
+  #get rid of extra ones eventually
 end
