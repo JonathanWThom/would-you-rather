38a93a504946b055baf394612b853824e7ffd0ff
Ezra Sandzer-Bell and Jonathan Thom <ezra@>
2017-02-20 11:24:48 -0800

    remove checkbox and vote button upon vote

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ActiveRecord::Base
-  validates :content, :option_1, :option_2, :presence => true
+  validates :content, :option_1, :option_2, :photo_1, :photo_2, :presence => true
   has_many :responses
 end
