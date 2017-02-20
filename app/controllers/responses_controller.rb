be8c0acc69bb2066148f1978cf07813ba99f45b9
Ezra Sandzer-Bell and Jonathan Thom <ezra+jonathan@>
2017-02-20 13:22:17 -0800

    Will add who you voted for

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,6 +4,12 @@ class ResponsesController < ApplicationController
     @response = @question.responses.new(option_1: params[:response][:option_1], option_2: params[:response][:option_2], user_id: current_user.id)
     if @response.save
       flash[:notice] = "You voted!"
+      @question_response = nil;
+      if @question.responses.where(user_id: current_user.id)[0].option_1
+        @question_response = @question.option_1
+      else
+        @question_response = @question.option_2
+      end
       respond_to do |format|
         format.html { redirect_to root_path }
         format.js
