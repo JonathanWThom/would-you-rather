cc5f8b4bd28ee5abc0724030890355e6ad412a81
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-20 09:30:24 -0800

    Add voting form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
+    @response = Response.new
   end
 end
--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,16 @@
 class ResponsesController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+    @response = @question.responses.new(params[:question_response])
+    if @response.save
+      flash[:notice] = "You voted!"
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        # format.js
+      end
+    else
+      flash[:notice] = "nope"
+      redirect_to root_path
+    end
+  end
 end
