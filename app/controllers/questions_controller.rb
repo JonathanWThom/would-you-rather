13f00ea2c6a335bc4f2ab6bdfa19b7a405b14276
Ezra Sandzer-Bell and Jonathan Thom <jonathan@>
2017-02-21 09:20:00 -0800

    append new question to root page using form partials

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,28 @@ class QuestionsController < ApplicationController
     @questions = Question.all
     @response = Response.new
   end
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @questions = Question.all
+    @question = Question.new(question_params)
+    @response = Response.new
+    if @question.save
+      flash[:notice] = "Question successfully added!"
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content, :option_1, :option_2, :photo_1, :photo_2)
+  end
 end
